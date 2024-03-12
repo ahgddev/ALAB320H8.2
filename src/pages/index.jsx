fff1dc38d625264ec46abfea12c788392789c6ac
ahgddev <ahgddev@gmail.com>
2024-03-11 19:25:19 -0700

    Error handling works.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,11 @@
 import ProcessShips from "../services/SW-Api";
 
 function GetShips() {
-//   const shipArr = [];
-//   for (let i = 0; i <= 11; i++) {
-//     shipArr.push(<ProcessShips shipNum={i} />);
-//   }
-//   return shipArr;
-return (
-    <>
-    <ProcessShips shipNum="0" />
-    <ProcessShips shipNum="1" />
-    <ProcessShips shipNum="2" />
-    <ProcessShips shipNum="3" />
-    <ProcessShips shipNum="4" />
-    <ProcessShips shipNum="5" />
-    <ProcessShips shipNum="6" />
-    <ProcessShips shipNum="7" />
-    <ProcessShips shipNum="8" />
-    <ProcessShips shipNum="9" />
-    <ProcessShips shipNum="10" />
-    </>
-)
+  const shipArr = [];
+  for (let i = 0; i <= 11; i++) {
+    shipArr.push(<ProcessShips shipNum={i} />);
+  }
+  return shipArr;
 }
 
 export default GetShips;
--- src/services/SW-Api.jsx
+++ src/services/SW-Api.jsx
@@ -1,27 +1,28 @@
 import { useState, useEffect } from "react";
-import axios from "axios"
+import axios from "axios";
 import StarshipCard from "../components/StarshipCard";
 
+function ProcessShips({ shipNum }) {
+  const [shipName, setShipName] = useState([]);
 
-function ProcessShips({shipNum}) {
-    const [shipName, setShipName] = useState([]);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        const response = await axios.get(`https://www.swapi.tech/api/starships/${shipNum}`);
-        setShipName(response.data.result.properties.name)
-        console.log(shipName)
-      };
-  
-      fetchData();
-    }, []);
-  
-    return (
-      <div>
-       <StarshipCard shipName={shipName} />
-      </div>
-    );
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios
+        .get(`https://www.swapi.tech/api/starships/${shipNum}`)
+        .catch(function (error) {
+          setShipName("A ship with this ID does not exist");
+        });
+      setShipName(response.data.result.properties.name);
+    };
 
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <StarshipCard shipName={shipName} />
+    </div>
+  );
 }
 
-export default ProcessShips
+export default ProcessShips;
